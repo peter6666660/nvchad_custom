3dfc3dda6b0ff2863525add476014e0d3d8f000c
Thone Liu <thorne_lew@126.com>
2023-05-04 08:04:19 +0800

    format lua_ls

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -27,7 +27,7 @@ local lsp_formatting = function(bufnr)
   vim.lsp.buf.format({
     filter = function(client)
       -- apply whatever logic you want (in this example, we'll only use null-ls)
-      return client.name == "null-ls"
+      return client.name == "null-ls" or client.name == 'lua_ls'
     end,
     bufnr = bufnr,
   })
