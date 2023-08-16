9f9b3b4b5ce33b4e7708aed0b7f78e9b2ad80121
peter <peter@peterdeMacBook-Pro.local>
2023-08-16 13:53:06 +0800

    feat: null-ls add prettier

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -10,11 +10,14 @@ local sources = {
 
   -- webdev stuff
   -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "javascript", "javascriptreact", "typescript",
-    "typescriptreact", } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    filetypes = { "html", "markdown", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  }, -- so prettier works only on these filetypes
 
   -- json
   b.formatting.fixjson,
+  -- prettier
+  b.formatting.prettier,
   -- Lua
   b.formatting.stylua,
 
@@ -24,13 +27,13 @@ local sources = {
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
+  vim.lsp.buf.format {
     filter = function(client)
       -- apply whatever logic you want (in this example, we'll only use null-ls)
-      return client.name == "null-ls" or client.name == 'lua_ls'
+      return client.name == "null-ls" or client.name == "lua_ls"
     end,
     bufnr = bufnr,
-  })
+  }
 end
 
 null_ls.setup {
@@ -38,8 +41,8 @@ null_ls.setup {
   sources = sources,
   -- 保存格式化
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
