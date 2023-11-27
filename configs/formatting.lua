697683c53071100f7e9f7da0567603619353cdce
peter <peter6666660@gmail.com>
2023-11-28 06:54:40 +0800

    add html -> prettier

--- configs/formatting.lua
+++ configs/formatting.lua
@@ -6,6 +6,7 @@ require("conform").setup {
   formatters_by_ft = {
     lua = { "stylua" },
     -- Use a sub-list to run only the first available formatter
+    html = { { "prettier" } },
     javascript = { { "prettier" } },
     javascriptreact = { { "prettier" } },
     typescript = { { "prettier" } },
